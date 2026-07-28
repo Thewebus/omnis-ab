ebb0f05e85f12734aad976c4768940a21c7cb784
SIDIBE Sidick c_ssidibe1 <sidick.sidibe@orange.com>
2026-07-28 16:45:32 +0000

    Signature place

--- resources/views/informatique/bulletin/bulletin-un-pdf.blade.php
+++ resources/views/informatique/bulletin/bulletin-un-pdf.blade.php
@@ -423,7 +423,7 @@
                             @elseif(env('OWNER') == 'ua_sp') San-Pedro,
                             @endif le {{ date('d/m/Y') }} <br>
                     </p>
-                    <p style="margin-left: 25rem; text-decoration: underline;">
+                    <p style="margin-left: 25rem; margin-top: -2rem; text-decoration: underline;">
                         {{ $signataire->fonction ?? 'Le Président du Conseil Scientifique' }}
                     </p>
                     <p style="margin-left: 25rem; margin-top:4rem; font-weight: bold;">
